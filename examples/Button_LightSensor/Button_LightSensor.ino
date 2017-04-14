497e10ba3e6ffeda2e88b0ab291cc90bf1c67f65
jerryyip <563355085@qq.com>
2017-04-14 11:16:48 +0800

    test

---
+++ examples/Button_LightSensor/Button_LightSensor.ino
@@ -0,0 +1,22 @@
+
+#include "MC20_Arduino_Interface.h"
+
+#define GrovePowerPin   12
+#define LightSensorPin  A0
+#define ButtonPin       2
+
+
+void setup() {
+  SerialUSB.begin(115200);
+  pinMode(ButtonPin, INPUT);
+  pinMode(GrovePowerPin, OUTPUT);
+  digitalWrite(GrovePowerPin, HIGH);   //write high to grove power pin to open power switch of grove port
+}
+
+void loop() {
+    if (digitalRead(ButtonPin)) {
+        SerialUSB.print("The value of light sensor:");
+        SerialUSB.println(analogRead(LightSensorPin));
+    }
+    delay(500);
+}   
